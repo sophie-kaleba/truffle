947f3172c24de96375220facb2c8dbc62e276237
Stefan Marr <git@stefan-marr.de>
2016-02-07 22:21:06 +0100

    Add helpers for on-demand instrumentation
    This is useful for dynamic languages where execution leads to node
    replacement, which requires new instrumentation.
    
    This change is a *hack*, and not supposed to be merged directly.
    
    Handled special cases:
    
    - unwrap wrapper nodes when inserting new wrappers
    - when there is already a wrapper node, just invalidate it
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>
    
    # Conflicts:
    #	truffle/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/InstrumentationHandler.java

--- truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/InstrumentationHandler.java
+++ truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/InstrumentationHandler.java
@@ -64,10 +64,12 @@ import com.oracle.truffle.api.source.SourceSection;
  * Central coordinator class for the Truffle instrumentation framework. Allocated once per
  * {@linkplain com.oracle.truffle.api.vm.PolyglotEngine engine}.
  */
-final class InstrumentationHandler {
+public final class InstrumentationHandler {
 
     /* Enable trace output to stdout. */
     private static final boolean TRACE = Boolean.getBoolean("truffle.instrumentation.trace");
+
+    private static InstrumentationHandler globalHandler;
 
     private final Object sourceVM;
 
@@ -105,6 +107,24 @@ final class InstrumentationHandler {
         this.out = out;
         this.err = err;
         this.in = in;
+        globalHandler = this;
+    }
+
+    public static void insertInstrumentationWrapper(Node instrumentableNode) {
+        insertInstrumentationWrapper(instrumentableNode, instrumentableNode.getSourceSection());
+    }
+
+    public static void insertInstrumentationWrapper(Node instrumentableNode, SourceSection sourceSection) {
+        assert globalHandler != null : "InstrumentationHandler not yet initialized";
+
+        Node node;
+        if (instrumentableNode instanceof WrapperNode) {
+            node = ((WrapperNode) instrumentableNode).getDelegateNode();
+            invalidateWrapperImpl((WrapperNode) instrumentableNode, node);
+        } else {
+            node = instrumentableNode;
+            globalHandler.insertWrapper(node, sourceSection);
+        }
     }
 
     Object getSourceVM() {
@@ -439,6 +459,7 @@ final class InstrumentationHandler {
 
     @SuppressWarnings("unchecked")
     private void insertWrapper(Node instrumentableNode, SourceSection sourceSection) {
+        assert !(instrumentableNode instanceof WrapperNode);
         final Node node = instrumentableNode;
         final Node parent = node.getParent();
         if (parent instanceof WrapperNode) {
