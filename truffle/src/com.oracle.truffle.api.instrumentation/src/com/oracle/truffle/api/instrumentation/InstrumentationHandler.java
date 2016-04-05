d7ea39a208b99adfe38f2ad377425a6c0a2f4d7e
Stefan Marr <git@stefan-marr.de>
2016-04-05 23:26:14 +0200

    Added isTaggedWith(Node,Class<?>) to Instrumenter
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/InstrumentationHandler.java
+++ truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/InstrumentationHandler.java
@@ -928,6 +928,11 @@ public final class InstrumentationHandler {
         }
 
         @Override
+        public boolean isTaggedWith(Node node, Class<?> tag) {
+            return AccessorInstrumentHandler.nodesAccess().isTaggedWith(node, tag);
+        }
+
+        @Override
         void verifyFilter(SourceSectionFilter filter) {
         }
 
@@ -1050,6 +1055,11 @@ public final class InstrumentationHandler {
         @Override
         public Set<Class<?>> queryTags(Node node) {
             return queryTagsImpl(node, languageInfo);
+        }
+
+        @Override
+        public boolean isTaggedWith(Node node, Class<?> tag) {
+            return AccessorInstrumentHandler.nodesAccess().isTaggedWith(node, tag);
         }
 
         @Override
