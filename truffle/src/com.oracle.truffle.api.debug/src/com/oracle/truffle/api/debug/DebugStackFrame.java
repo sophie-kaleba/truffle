3a4d3dded08b5a4227e4370d52590650d2021fed
Stefan Marr <git@stefan-marr.de>
2016-11-22 22:35:49 +0100

    Added accessors for frame and root node in DebugStackFrame
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/DebugStackFrame.java
+++ truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/DebugStackFrame.java
@@ -335,4 +335,11 @@ public final class DebugStackFrame {
         event.verifyValidState(allowDifferentThread);
     }
 
+    public MaterializedFrame getFrame() {
+        return findTruffleFrame();
+    }
+
+    public RootNode getRootNode() {
+        return findCurrentRoot();
+    }
 }
