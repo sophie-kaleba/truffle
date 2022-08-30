2b1173e21a31f7c9c226f52a63f49c7409282a0d
Josef Haider <josef.haider@oracle.com>
2022-08-30 12:09:08 +0200

    TruffleStrings: ensure strings created by FromByteArrayNode and FromNativePointerNode can be virtualized.

--- compiler/mx.compiler/suite.py
+++ compiler/mx.compiler/suite.py
@@ -29,6 +29,13 @@ suite = {
          ]
       },
       {
+        "name" : "regex",
+        "subdir": True,
+        "urls" : [
+          {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
+         ]
+      },
+      {
         "name" : "java-benchmarks",
         "subdir": True
       }
@@ -1952,11 +1959,13 @@ suite = {
         "GRAAL_ONLY_TEST",
         "truffle:TRUFFLE_SL_TEST",
         "truffle:TRUFFLE_TEST",
+        "regex:TREGEX"
       ],
       "exclude" : [
         "mx:JUNIT",
         "JAVA_ALLOCATION_INSTRUMENTER",
       ],
+      "testDistribution" : True,
       "maven": False,
     },
 
