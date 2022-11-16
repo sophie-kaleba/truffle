c4c586efa56579a607f95f53de83b08b7b0d666c
Tomas Zezula <tomas.zezula@oracle.com>
2022-11-16 10:33:33 +0100

    [GR-42271] Fixed review comments.

--- truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/EngineAccessor.java
+++ truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/EngineAccessor.java
@@ -1980,11 +1980,6 @@ final class EngineAccessor extends Accessor {
             }
             return new LanguageSystemThread(languageContext, runnable, threadGroup);
         }
-
-        @Override
-        public String isSafeToPreinitialize(Path path) {
-            return FileSystems.isSafeToPreinitialize(path);
-        }
     }
 
     abstract static class AbstractClassLoaderSupplier implements Supplier<ClassLoader> {
