3bb7e5a7e1b01e51325451134adfe3c18457fdb2
Tomas Zezula <tomas.zezula@oracle.com>
2022-11-22 18:46:18 +0100

    [GR-42414] Fixed review comments.

--- sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/impl/AbstractPolyglotImpl.java
+++ sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/impl/AbstractPolyglotImpl.java
@@ -1202,7 +1202,8 @@ public abstract class AbstractPolyglotImpl {
 
     public abstract static class LogHandler {
 
-        protected LogHandler() {
+        protected LogHandler(AbstractPolyglotImpl polyglot) {
+            Objects.requireNonNull(polyglot);
         }
 
         public abstract void publish(LogRecord logRecord);
