1464de3f37891ba92203c6d315407ad81001be5b
Sophie Kaleba <sophie.kaleba@gmail.com>
2023-01-26 16:42:52 +0000

    Delete out of used methods

--- compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedDirectCallNode.java
+++ compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedDirectCallNode.java
@@ -56,11 +56,6 @@ public final class OptimizedDirectCallNode extends DirectCallNode implements Tru
         assert target.isSourceCallTarget();
     }
 
-    OptimizedDirectCallNode(OptimizedCallTarget target, Object[] userArgs) {
-        super(target, userArgs);
-        assert target.isSourceCallTarget();
-    }
-
     @Override
     public Object call(Object... arguments) {
         OptimizedCallTarget target = getCurrentCallTarget();
